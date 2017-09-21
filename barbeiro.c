358980a30b879d7ea4492ac3be54cfb7aa52e3ad
Fernando Mendes <fernandomendes@id.uff.br>
2017-09-21 15:44:09 -0300

    Algoritmo do slide implementado.

--- barbeiro.c
+++ barbeiro.c
@@ -1,39 +1,59 @@
+#define CHAIRS 5
 
 #include <stdio.h>
 #include <pthread.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <semaphore.h>
 
-typedef int semaphore;
-
-semaphore customers = 0;
-semaphore barbers = 0;
-semaphore painters = 0;
-semaphore hairstylist = 0;
-semaphore mutex = 1;
+sem_t customers, barbers,
+      painters, hairstylist,
+      mutex;
+    
 int waiting = 0;
 
 
+void shave(){
+    printf("Ta feita...");
+}
+
+void get_shave(){
+    printf("To indo fazer a barba hein...");
+}
+
 void barber(){
     while(1){
-        down(&customers);
-        down(&mutex);
+        sem_wait(&customers);
+        sem_wait(&mutex);
         waiting = waiting - 1;
-        up(&barbers);
-        up(&mutex);
-        cut_beard();
+        sem_post(&barbers);
+        sem_post(&mutex);
+        shave();
     }
 
 }
 
-void painter(){
-    while(1){
-        down(&customers);
+void customer(){
+    sem_wait(&mutex);
+    if(waiting < CHAIRS){
+        waiting = waiting + 1;
+        sem_post(&customers);
+        sem_post(&mutex);
+        sem_wait(&barbers);
+        get_shave();
+    } else {
+        sem_post(&mutex);
     }
-
-
 }
+
 int main(){
+
+    sem_init(&customers, 0, 0);
+    sem_init(&barbers, 0, 0);
+    sem_init(&painters, 0, 0);
+    sem_init(&hairstylist, 0, 0);
+    sem_init(&mutex, 0, 1);
     printf("Olá Barbeiro\n");
     return 0;
 }
